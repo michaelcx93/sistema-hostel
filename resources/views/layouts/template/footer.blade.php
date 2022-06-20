42c38819f1e60a93d346bbda60dc9789480ab006
michaelcx93 <michael.calix@outlook.es>
2022-06-19 20:38:36 -0600

    Primer commit

---
+++ resources/views/layouts/template/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+        <div class="float-right d-none d-sm-block">
+        </div>
+        <strong>Copyright &copy; 2022 <a href="https://www.linkedin.com/in/michael-calix-b07303155/">Michal.Calix</a>.</strong> All rights reserved.
+      </footer>
